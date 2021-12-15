af5207a0aff0af72489340445fa79ea0c11e2ed8
Vladimir Babsushkin <zzzzzzz@zzzz.rer>
2021-12-15 16:37:53 +0700

    files work

--- Lesson-19-file-work.py
+++ Lesson-19-file-work.py
@@ -1,8 +1,14 @@
-inputfile = '/home/vladimir/Myproject/Piton/user_names.txt'
+inputfile = '/home/vladimir/Myproject/Piton/user_passwords.txt'
+outputfile = '/home/vladimir/Myproject/Piton/my_passwords.txt'
+
+password_tolookfor = "123"
 myfile = open(inputfile, mode='r', encoding='ascii')
-# print(myfile.read())
+# print(myfile.read())                                      # a-append add
+myfile2 = open(outputfile, mode='a', encoding='ascii')      # w-perazapis
 
 for num, line in enumerate(myfile, 1):
-    if "Davidson" in line:
+    if password_tolookfor in line:
         print("Line N:" + str(num) + " : " + line.strip())
-
+        myfile2.write("Found password" + line)
+myfile.close()
+myfile2.close()
