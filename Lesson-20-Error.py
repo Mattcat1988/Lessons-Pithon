d42781a468b0732b1dc5e94a4e2420e161918b8b
Vladimir Babsushkin <zzzzzzz@zzzz.rer>
2021-12-16 10:46:57 +0700

    error

--- Lesson-20-Error.py
+++ Lesson-20-Error.py
@@ -1,1 +1,16 @@
-file
+
+filename = "Lesson_list.txt"
+while True:
+    try:
+        print("Inside TRY")
+        myfile = open(filename, mode='r', encoding="Latin-1")
+    except Exception:
+        print("Inside EXCEPT")
+        print("Error Found!")
+        filename = input("Enter Correct file name! :")
+    else:
+        print("Inside ELSE")
+        print(myfile.read())
+    finally:
+        print("Inside Finally")
+        print("=============end of=============")
