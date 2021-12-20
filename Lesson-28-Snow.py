888307f31223f658149b7d41b5495a7c16fff0aa
Vladimir Babsushkin <zzzzzzz@zzzz.rer>
2021-12-20 16:31:01 +0700

    work

--- Lesson-28-Snow.py
+++ Lesson-28-Snow.py
@@ -3,11 +3,10 @@ import random
 import sys
 import time
 
-MAX_X = 1920
-MAX_Y = 1080
+MAX_X = 1366
+MAX_Y = 768
 MAX_SNOW = 100
 SNOW_SIZE = 64
-
 
 class Snow():
     def __init__(self, x, y):
@@ -25,11 +24,10 @@ class Snow():
             self.y = (0 - SNOW_SIZE)
 
         i = random.randint(1, 3)
-        if i == 1:  # Move RIGHT
+        if i == 1:   # Move RIGHT
             self.x += 1
             if self.x > MAX_X:
                 self.x = (0 - SNOW_SIZE)
-
         elif i == 2:  # Move LEFT
             self.x -= 1
             if self.x < (0 - SNOW_SIZE):
@@ -45,20 +43,18 @@ def initilize_snow(max_snow, snowfall):
         yy = random.randint(0, MAX_Y)
         snowfall.append(Snow(xx, yy))
 
-
 def check_for_exit():
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             sys.exit()
 
+# ------------------------- MAIN ----------------------
 
-# ------------------MAIN------------------
-# def initialize_pygame(MAX_X, MAX_Y):
 pygame.init()
 
 screen = pygame.display.set_mode((MAX_X, MAX_Y), pygame.FULLSCREEN)
 
-bg_color = (0, 0, 0)
+bg_color = (0 ,0 ,0)
 snowfall = []
 
 initilize_snow(MAX_SNOW, snowfall)
@@ -68,6 +64,6 @@ while True:
     check_for_exit()
     for i in snowfall:
         i.move_snow()
-        i.move_snow()
+        i.draw_snow()
     time.sleep(0.0020)
     pygame.display.flip()
